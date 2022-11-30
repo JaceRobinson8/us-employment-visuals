6ede0068140de6784585e06fff5b432ae379e470
Alex Pickering <alex.pickering15@gmail.com>
2022-11-30 12:55:42 -0500

    Added todo list with @s

--- us_econ/main.py
+++ us_econ/main.py
@@ -1,6 +1,17 @@
 from bokeh.plotting import figure, show
 
+#TODO: @jr
+#TODO: @apick
+# [ ] bokeh tutorial
+# [ ] using dataset
+#   - [ ] craft hypothesis
+#   - [ ] prove or disprove hypothesis based on data
+# [ ] visualize the data
+#   - [ ] one way
+#   - [ ] different way
+
 def main():
+
     print("hello world")
 
 if __name__ == "__main__":
